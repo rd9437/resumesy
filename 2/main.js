19c2f88381f30671edb78a8df6b2eda57b72e0fa
Rudransh Das <143277515+rd9437@users.noreply.github.com>
2024-04-05 22:54:19 +0530

    Create main.js

---
+++ 2/main.js
@@ -0,0 +1,32 @@
+function downloadimage() {
+  var container = document.getElementById("doc2");
+  window.scrollTo(0,0); 
+  html2canvas(container, {scrollY: -window.scrollY}, {useCORS:true}).then(function(canvas) {
+      var link = document.createElement("a");
+      document.body.appendChild(link);
+      link.download = "resme.png";
+      link.href = canvas.toDataURL();
+      link.target = '_blank';
+      link.click();
+  });
+}
+
+function showPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+  document.cookie = "popupShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+}
+
+// Check if the popup has been shown before
+function checkPopupCookie() {
+  var popupShown = document.cookie.replace(/(?:(?:^|.*;\s*)popupShown\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  if (popupShown !== "true") {
+      showPopup();
+  }
+}
+
+window.onload = checkPopupCookie;
